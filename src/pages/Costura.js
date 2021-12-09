7a580069c2512ff726d3e21b52835886fc4f4f4a
Weverson Santos de Oliveira Queiroz <weverson.queiroz.dev@gmail.com>
2021-12-09 17:09:27 -0300

    concluido

--- src/pages/Costura.js
+++ src/pages/Costura.js
@@ -24,24 +24,45 @@ class Costura extends Component{
     if (prd?.length > 3) {
       prd.length = 3;
     }
-    
+    console.log(list)
+    console.log(prd)
     if (list?.length > 0){
       return(
+        
         <center>
         <div>
           
           {list?.map(({fase, produto,qtde, cod_produto}) => (
             <ul key={fase}>
-              <h1 style={{borderStyle: 'solid' ,fontSize: '50px' ,marginTop: '15px', width: '900px',color: 'Black'}}>FASE : {fase}</h1>            
-              <ul style={{borderStyle: 'solid', fontSize: '40px' , marginTop: '15px', width: '900px', height : '180px', color: 'Black'}}><u>PRODUTO : </u><br/>{produto}<br/>{cod_produto}</ul>
-              <b><ul style={{borderStyle: 'solid' ,fontSize: '30px' , width: '400px', color: 'Black'}}>QUANTIDADE: {qtde}</ul></b>
-              <ul style={{fontSize: '30px', marginTop: '15px',}}>Proximos Produtos:<br/> </ul>
-              <h3 style={{borderStyle: 'solid' ,marginTop: '15px', width: 'fit-content', color: 'Black', backgroundColor: '#15CDFC'}}> {prd?.map(({produto}) => (
+              <h1 style={{borderStyle: 'solid' ,fontSize: '80px' ,marginTop: '15px', width: '1200px',height : '100px',color: 'Black'}}>FASE : {fase}</h1>            
+              <ul style={{borderStyle: 'solid', fontSize: '80px' , marginTop: '15px', width: '1200px', height : '300px', color: 'Black'}}><u>PRODUTO : </u><br/>{produto}<br/>{cod_produto}</ul>
+              <b><ul style={{borderStyle: 'solid' ,fontSize: '50px' , width: '500px', color: 'Black'}}>QUANTIDADE: {qtde}</ul></b>
+              <b><ul style={{fontSize: '30px', marginTop: '35px',}}>Proximos Produtos:<br/> </ul></b>
+              <table style={{ marginTop: '25px',}}>
+                <b>
+                <thead>
+                  <tr>
+                    <th style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(0 114 245)'}}>Posição</th>
+                    <th style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(0 114 245)'}}>Nome</th>
+                    <th style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(0 114 245)'}}>Código</th>
+                    <th style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(0 114 245)'}}>Quantidade</th>
+                  </tr>
+                </thead>
+                <tbody>
+                
+                {prd?.map(({produto, cod_produto, qtde},index) => (
+                
+                    <tr key={produto}>
+                      <td style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(151 189 233)',width: '300px',textAlign: 'center' , height : '50px'}}>{index+1}</td>
+                      <td style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(151 189 233)',width: '600px',textAlign: 'center'}}>{produto}</td>
+                      <td style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(151 189 233)',width: '300px',textAlign: 'center'}}>{cod_produto}</td>
+                      <td style={{borderStyle: '1px',fontSize: '25px', backgroundColor: 'rgb(151 189 233)',width: '300px',textAlign: 'center'}}>{qtde}</td>
+                    </tr>
+                  ))}
+                </tbody>
+                </b>
+              </table>
               
-              <ul> {produto}</ul>
-             
-              
-              ))} </h3>
             </ul> 
             
           ))}
